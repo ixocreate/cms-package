96130e3b6b56af0e7d184714e1ddb33950c0bc9b
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-06-01 14:27:53 +0200

    added navigation endpoints

---
+++ src/Metadata/NavigationMetadata.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * kiwi-suite/cms (https://github.com/kiwi-suite/cms)
+ *
+ * @package kiwi-suite/cms
+ * @see https://github.com/kiwi-suite/cms
+ * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace KiwiSuite\Cms\Metadata;
+
+use Doctrine\DBAL\Types\Type;
+use KiwiSuite\Database\ORM\Metadata\AbstractMetadata;
+use Doctrine\ORM\Mapping\Builder\FieldBuilder;
+use KiwiSuite\CommonTypes\Entity\UuidType;
+
+final class NavigationMetadata extends AbstractMetadata
+{
+
+    protected function buildMetadata(): void
+    {
+        $builder = $this->getBuilder();
+        $builder->setTable('cms_navigation');
+
+        $this->setFieldBuilder('id',
+            $builder->createField('id', UuidType::class)
+                ->makePrimaryKey()
+        )->build();
+
+        $this->setFieldBuilder('pageId',
+            $builder->createField('pageId', UuidType::class)
+        )->build();
+
+        $this->setFieldBuilder('navigation',
+            $builder->createField('navigation', Type::STRING)
+        )->build();
+    }
+    
+    public function id(): FieldBuilder
+    {
+        return $this->getField('id');
+    }
+
+    public function pageId(): FieldBuilder
+    {
+        return $this->getField('pageId');
+    }
+
+    public function content(): FieldBuilder
+    {
+        return $this->getField('navigation');
+    }
+}
+
---
+++ src/Middleware/LoadPageMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+namespace KiwiSuite\Cms\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Zend\Expressive\Router\RouteResult;
+
+final class LoadSitemapMiddleware implements MiddlewareInterface
+{
+
+    /**
+     * Process an incoming server request and return a response, optionally delegating
+     * response creation to a handler.
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        /** @var RouteResult $routeResult */
+        $routeResult = $request->getAttribute(RouteResult::class);
+
+        $pageId = $routeResult->getMatchedRoute()->getOptions()['pageId'];
+    }
+}

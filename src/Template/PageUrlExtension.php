4121ff9cb3b6ffd9bd6df3410787fad959f2ba22
Lucas Seinfeld <l.seinfeld@kiwi-suite.com>
2018-07-30 20:53:42 +0200

    page url from handle

--- src/Template/PageUrlExtension.php
+++ src/Template/PageUrlExtension.php
@@ -1,26 +1,42 @@
 <?php
+
 namespace KiwiSuite\Cms\Template;
 
 use KiwiSuite\Cms\Entity\Page;
+use KiwiSuite\Cms\Repository\PageRepository;
+use KiwiSuite\Cms\Repository\SitemapRepository;
 use KiwiSuite\Cms\Router\PageRoute;
 use KiwiSuite\Contract\Template\ExtensionInterface;
 
 final class PageUrlExtension implements ExtensionInterface
 {
-
-
     /**
      * @var PageRoute
      */
     private $pageRoute;
+    /**
+     * @var SitemapRepository
+     */
+    private $sitemapRepository;
+
+    /**
+     * @var PageRepository
+     */
+    private $pageRepository;
 
     /**
      * PageUrlExtension constructor.
+     *
      * @param PageRoute $pageRoute
      */
-    public function __construct(PageRoute $pageRoute)
-    {
+    public function __construct(
+        PageRoute $pageRoute,
+        SitemapRepository $sitemapRepository,
+        PageRepository $pageRepository
+    ) {
         $this->pageRoute = $pageRoute;
+        $this->sitemapRepository = $sitemapRepository;
+        $this->pageRepository = $pageRepository;
     }
 
     /**
@@ -48,5 +64,25 @@ final class PageUrlExtension implements ExtensionInterface
     {
         return $this->pageRoute->fromPage($page, $params, $locale);
     }
+
+    /**
+     * @param Page  $page
+     * @param array $params
+     * @return string
+     */
+    public function fromHandle(string $handle, array $params = [], ?string $locale = null): string
+    {
+        $sitemap = $this->sitemapRepository->findOneBy(['handle' => $handle]);
+        if (!$sitemap) {
+            return null;
+        }
+        $page = $this->pageRepository->findOneBy([
+            'sitemapId' => $sitemap->id(),
+            'locale'    => $locale ? $locale : \Locale::getDefault(),
+        ]);
+        if (!$page) {
+            return null;
+        }
+        return $this->fromPage($page, $params, $locale);
     }
 }

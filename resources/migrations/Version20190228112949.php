865c8e33d420f41e3d7f9cf95942d35df5472a19
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2019-03-19 20:25:56 +0100

    added missing downgrade path for migrations

--- resources/migrations/Version20190228112949.php
+++ resources/migrations/Version20190228112949.php
@@ -22,7 +22,6 @@ final class Version20190228112949 extends AbstractMigration
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-
+        $schema->dropTable("cms_redirect_page");
     }
 }

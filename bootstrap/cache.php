3215086865026f9ccaef82844c295964476ce9ee
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2019-04-12 15:18:30 +0200

    switch to cache bootstrap

--- bootstrap/cache.php
+++ bootstrap/cache.php
@@ -3,7 +3,14 @@ declare(strict_types=1);
 
 namespace Ixocreate\Cms;
 
-use Ixocreate\Package\Cache\CacheableConfigurator;
+use Ixocreate\Package\Cache\CacheConfigurator;
+use Ixocreate\Package\Cache\Option\Chain;
+use Ixocreate\Package\Cache\Option\Filesystem;
+use Ixocreate\Package\Cache\Option\InMemory;
 
-/** @var CacheableConfigurator $cache */
-$cache->addDirectory(__DIR__ . '/../src/Cacheable');
+/** @var CacheConfigurator $cache */
+$cache->addCacheableDirectory(__DIR__ . '/../src/Cacheable');
+
+$cache->addCache('cms', new Chain(['cms_tmp', 'cms_store']));
+$cache->addCache('cms_tmp', new InMemory());
+$cache->addCache('cms_store', new Filesystem());

801f0db3ca0a63d1f948f0f03523ef17da6483b8
Josef Jost <44982723+JJosef84@users.noreply.github.com>
2019-04-25 09:51:12 +0200

    copy admin tree

---
+++ src/Site/Admin/AdminContainerInterface.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOCREATE GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ixocreate\Cms\Site\Admin;
+
+interface AdminContainerInterface extends \RecursiveIterator, \Countable
+{
+    /**
+     * @param string|callable $filter
+     * @return AdminContainerInterface
+     */
+    public function filter($filter, array $params = []): self;
+
+    /**
+     * @param int $level
+     * @return AdminContainerInterface
+     */
+    public function withMaxLevel(int $level): self;
+
+    /**
+     * @param string $navigation
+     * @return AdminContainerInterface
+     */
+    public function withNavigation(string $navigation): self;
+
+    /**
+     * @param string|callable $filter
+     * @return AdminContainerInterface
+     */
+    public function where($filter, array $params = []): self;
+
+    /**
+     * @param int $level
+     * @return AdminContainerInterface
+     */
+    public function withMinLevel(int $level): self;
+
+    /**
+     * @return AdminContainerInterface
+     */
+    public function flatten(): self;
+
+    /**
+     * @param string|callable $filter
+     * @return AdminItem|null
+     */
+    public function find($filter, array $params = []): ?AdminItem;
+
+    /**
+     * @param string $handle
+     * @return AdminItem|null
+     */
+    public function findByHandle(string $handle): ?AdminItem;
+
+    /**
+     * @param callable $callable
+     * @return AdminContainerInterface
+     */
+    public function sort(callable $callable): self;
+}

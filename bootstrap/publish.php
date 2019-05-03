457b1a0a010866c7d440724c5caff3dc9f877741
Bernhard Efler <1978420+bernhard-efler@users.noreply.github.com>
2019-05-03 13:17:02 +0200

    prepare for application 0.2

--- bootstrap/publish.php
+++ bootstrap/publish.php
@@ -12,4 +12,4 @@ namespace Ixocreate\Cms;
 /** @var PublishConfigurator $publish */
 use Ixocreate\Application\Publish\PublishConfigurator;
 
-$publish->add('migrations', __DIR__ . '/../resources/migrations');
+$publish->addSource('migrations', __DIR__ . '/../resources/migrations');

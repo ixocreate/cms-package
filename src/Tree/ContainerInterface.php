a26be49472e5097fe52b862d4e886433544a6121
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2019-06-26 13:26:02 +0200

    missing methods in interface

--- src/Tree/ContainerInterface.php
+++ src/Tree/ContainerInterface.php
@@ -10,8 +10,21 @@ declare(strict_types=1);
 namespace Ixocreate\Cms\Tree;
 
 use Countable;
+use Ixocreate\Cms\Tree\Structure\Structure;
+use Ixocreate\Collection\CollectionInterface;
 use RecursiveIterator;
 
 interface ContainerInterface extends Countable, RecursiveIterator
 {
+    public function structure(): Structure;
+
+    public function find($filter, array $params = []): ?ItemInterface;
+
+    public function where($filter, array $params = []): ContainerInterface;
+
+    public function flatten(): CollectionInterface;
+
+    public function search($filter, array $params = []): CollectionInterface;
+
+    public function filter($filter, array $params = []): ContainerInterface;
 }
--- src/Tree/ItemInterface.php
+++ src/Tree/ItemInterface.php
@@ -9,6 +9,32 @@ declare(strict_types=1);
 
 namespace Ixocreate\Cms\Tree;
 
+use Ixocreate\Cms\Entity\Page;
+use Ixocreate\Cms\Entity\Sitemap;
+use Ixocreate\Cms\PageType\PageTypeInterface;
+use Ixocreate\Cms\Tree\Structure\StructureItem;
+
 interface ItemInterface extends ContainerInterface
 {
+    public function sitemap(): Sitemap;
+
+    public function hasPage(string $locale = null): bool;
+
+    public function page(string $locale): Page;
+
+    public function isOnline(string $locale): bool;
+
+    public function pageType(): PageTypeInterface;
+
+    public function handle(): ?string;
+
+    public function navigation(string $locale): array;
+
+    public function level(): int;
+
+    public function parent(): ?ItemInterface;
+
+    public function structureItem(): StructureItem;
+
+    public function below(): ContainerInterface;
 }
